7996a8e4f93e14da6b248e72cff11c986cbe7452
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-05 10:19:38 +0100

    Change the color of the body of the page on click

---
+++ Level1/Challenge1/main.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById('L1C1_btn');
+let bodyColor = document.getElementById('body');
+
+btn.addEventListener("click", function() {
+    bodyColor.style.background = 'yellow';
+});
