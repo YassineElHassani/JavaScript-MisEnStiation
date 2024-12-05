b5480449e2238f6f0c528a1200e0d0d82e653c78
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-05 16:59:53 +0100

    Change the value of an item on mouse hover

---
+++ Level1/Challenge7/main.js
@@ -0,0 +1,9 @@
+function bHover(b) {
+    b.style.cursor = "pointer";
+    b.src = "../../images/aHover.png";
+}
+
+function aHover(a) {
+    a.style.cursor = "pointer";
+    a.src = "../../images/bHover.png";
+}
